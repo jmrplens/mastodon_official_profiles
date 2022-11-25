9a91b1a6d66496994edb1087403420a1214b989e
Jose M. Requena Plens <joreple@upv.es>
2022-11-25 01:24:55 +0100

    Auto CSV to Readme with Python

---
+++ CSVtoReadme.py
@@ -0,0 +1,25 @@
+# CSV to Markdown in desired locations
+# Jose M. Requena Plens
+# Use the html comments to write markdown tables
+# Between:
+# <!-- CSV start filepath -->
+# <!-- CSV end -->
+
+import glob
+import pandas as pd  # To import CSV and Markdown conversion
+from minsert import MarkdownFile  # To insert Markdown in md.file
+
+# Get all CSV Filepaths
+result = glob.glob('*/**.csv')
+
+# Loop over files to create dictionary (Key=filepath, Value=markdown table)
+dict_csv = {}
+for file in result:
+    # Create Keys with filepaths and values with Markdown table
+    df = pd.read_csv(file)
+    dict_csv[file] = df.to_markdown(index = False)
+
+# Get .md File
+file = MarkdownFile("README.md")
+# Refresh tables
+file.insert(dict_csv)
